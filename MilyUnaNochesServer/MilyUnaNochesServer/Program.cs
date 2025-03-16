c4a9d62922f9bd92df330b70959381c7b1bc465e
Juan-Cumplido <zs22020936@estudiantes.uv.mx>
2025-03-16 01:43:04 -0600

    Inicio del proyecto

---
+++ MilyUnaNochesServer/MilyUnaNochesServer/Program.cs
@@ -0,0 +1,27 @@
+﻿using MilyUnaNochesService.Contracts;
+using MilyUnaNochesService.Services;
+using MilyUnaNochesService.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilyUnaNochesServer
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(MilyUnaNochesService.Services.MilyUnaNochesService)))
+            {
+                host.Open();
+                Console.WriteLine("Service connected");
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
---
+++ MilyUnaNochesServer/MilyUnaNochesService/Contracts/IUserManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+namespace MilyUnaNochesService.Contracts
+{
+    [ServiceContract]
+    public interface IUserManager
+    {
+        /// <summary>
+        /// Obtains a user profile by his username
+        /// </summary>
+        /// <param name="profile">Contains the data of the profile to add</param>
+        /// <returns>Returns 1 if the insertion was succesfull</returns>
+        [OperationContract]
+        int AddUser(Profile profile);
+    }
+
+    [DataContract]
+    public class Profile : AccessAccount
+    { 
+    }
+
+    [DataContract]
+    public class AccessAccount
+    {
+        [DataMember]
+        public int idAcceso { get; set; }
+    }
+
+}
