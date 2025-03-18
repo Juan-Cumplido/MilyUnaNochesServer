a7889c097834ec17788d8b1d283020b264309c3c
Christian Alberto Vázquez Cruz <zS22013636@estudiantes.uv.mx>
2025-03-18 15:57:13 -0600

    Funcional CU Registrar Proveedor

--- MilyUnaNochesServer/DataBaseManager/Program.cs
+++ MilyUnaNochesServer/DataBaseManager/Program.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataBaseManager.Operations {
-    internal class UserOperation {
+namespace DataBaseManager {
+    internal class Program {
+        static void Main(string[] args) {
+        }
     }
 }
