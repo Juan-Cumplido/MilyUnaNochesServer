b36cd70bb357b58d2d391f626c4be9070585c2a7
Juan-Cumplido <zs22020936@estudiantes.uv.mx>
2025-03-18 11:24:18 -0600

    servicios para el cliente

--- MilyUnaNochesServer/DataBaseManager/Operations/UserOperation.cs
+++ MilyUnaNochesServer/DataBaseManager/Operations/UserOperation.cs
@@ -1,12 +1,104 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MilyUnaNochesService.Utilities;
+using System.Data.Entity.Core;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web.ModelBinding;
+using System;
 
 namespace DataBaseManager.Operations
 {
-    internal class UserOperation
+    public class UserOperation
     {
+        public int addClient(Usuario user)
+        {
+            LoggerManager logger = new LoggerManager(this.GetType());
+            int result = Constants.ErrorOperation;
+            try
+            {
+                using (var dataBaseContext = new MilYUnaNochesEntities())
+                {
+                    using (var dataBaseContextTransaction = dataBaseContext.Database.BeginTransaction())
+                    {
+                        try
+                        {
+                            
+                            var newUser = new Usuario
+                            {
+                                nombre = user.nombre,
+                                primerApellido = user.primerApellido,
+                                segundoApellido = user.segundoApellido,
+                                correo = user.correo,
+                                telefono = user.telefono,
+                                estadoUsuario = user.estadoUsuario,
+                            };
+                            dataBaseContext.Usuario.Add(newUser);
+                            dataBaseContext.SaveChanges();
+                            int lastIdUserInserted = newUser.idUsuario;
+
+                            var newClient = new Cliente
+                            {
+                                idUsuario = lastIdUserInserted,
+                            };
+                            dataBaseContext.Cliente.Add(newClient);
+                            dataBaseContext.SaveChanges();
+                            
+                           
+                            dataBaseContextTransaction.Commit();
+                            result = Constants.SuccessOperation;
+                        }
+                        catch (DbUpdateException updateException)
+                        {
+                            logger.LogWarn(updateException);
+                            dataBaseContextTransaction.Rollback();
+                            result = Constants.ErrorOperation;
+                        }
+                        catch (SqlException sqlException)
+                        {
+                            logger.LogError(sqlException);
+                            dataBaseContextTransaction.Rollback();
+                            result = Constants.ErrorOperation;
+                        }
+                    }
+                }
+            }
+            catch (EntityException entityException)
+            {
+                logger.LogFatal(entityException);
+            }
+            return result;
+        }
+
+        public int VerifyExistingClientIntoDataBase(string name, string firstLastName, string secondLastName)
+        {
+            int verificationResult = -1;
+            LoggerManager logger = new LoggerManager(this.GetType());
+            try
+            {
+                using (var dataBaseContext = new MilYUnaNochesEntities())
+                {
+                    var existingClient = dataBaseContext.Usuario.FirstOrDefault(client => client.nombre == name && client.primerApellido == firstLastName && client.segundoApellido == secondLastName);
+                    if (existingClient != null)
+                    {
+                        verificationResult = Constants.DataMatches;
+                    }
+                    else
+                    {
+                        verificationResult = Constants.NoDataMatches;
+                    }
+                }
+            }
+            catch (SqlException sqlException)
+            {
+                logger.LogError(sqlException);
+                verificationResult = Constants.ErrorOperation;
+            }
+            catch (EntityException entityException)
+            {
+                logger.LogFatal(entityException);
+                verificationResult = Constants.ErrorOperation;
+            }
+            return verificationResult;
+        }
     }
 }
--- MilyUnaNochesServer/MilyUnaNochesService/Contracts/IUserManager.cs
+++ MilyUnaNochesServer/MilyUnaNochesService/Contracts/IUserManager.cs
@@ -7,25 +7,63 @@ namespace MilyUnaNochesService.Contracts
     [ServiceContract]
     public interface IUserManager
     {
-        /// <summary>
-        /// Obtains a user profile by his username
-        /// </summary>
-        /// <param name="profile">Contains the data of the profile to add</param>
-        /// <returns>Returns 1 if the insertion was succesfull</returns>
         [OperationContract]
-        int AddUser(Profile profile);
+        int AddClient(Usuario usuario);
+
+
+        [OperationContract]
+        bool VerifyAccess(string username, string password);
+
+        [OperationContract]
+        int VerifyExistinClient(string name,string firstLastName, string secondLastName);
     }
 
     [DataContract]
-    public class Profile : AccessAccount
-    { 
+    public class Usuario
+    {
+        [DataMember]
+        public int idUsuario { get; set; }
+        [DataMember]
+        public string nombre { get; set; }
+        [DataMember]
+        public string primerApellido { get; set; }
+        [DataMember]
+        public string segundoApellido { get; set; }
+        [DataMember]
+        public string correo { get; set; }
+        [DataMember]
+        public string telefono { get; set; }
+        [DataMember]
+        public string estadoUsuario { get; set; }
+
     }
 
     [DataContract]
-    public class AccessAccount
+    public class Acceso
     {
         [DataMember]
         public int idAcceso { get; set; }
+        [DataMember]
+        public string idEmpleado { get; set; }
+        [DataMember]
+        public string username { get; set; }
+        [DataMember]
+        public string password { get; set; }
+       
     }
 
+    [DataContract]
+    public class Direccion
+    {
+        [DataMember]
+        public int idDireccion { get; set; }
+        [DataMember]
+        public string calle { get; set; }
+        [DataMember]
+        public string numero { get; set; }
+        [DataMember]
+        public string codigoPostal { get; set; }
+        [DataMember]
+        public string ciudad { get; set; }
+    }
 }
