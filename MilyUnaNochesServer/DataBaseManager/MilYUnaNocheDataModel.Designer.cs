7fab6c76e0f9a80b5c17b9bb23fb29c16e91a3f4
Juan-Cumplido <zs22020936@estudiantes.uv.mx>
2025-04-06 23:45:29 -0600

    cambios en el appconfig

--- MilyUnaNochesServer/DataBaseManager/MilYUnaNocheDataModel.Designer.cs
+++ MilyUnaNochesServer/DataBaseManager/MilYUnaNocheDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'D:\Trabajos de Erick\LIS UV\6 SEXTO SEMESTRE\Desarrollo de Software\Mil y Una Noches Server\MilyUnaNochesServer\DataBaseManager\MilYUnaNocheDataModel.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\cumpl\OneDrive\Escritorio\PROYECTO-MILYUNANOCHES\MilyUnaNochesServer\MilyUnaNochesServer\DataBaseManager\MilYUnaNocheDataModel.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
